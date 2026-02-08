b88e201d167ab034a394eebad6aca98c876d3ae9
Weltogeisto <hendriksteinort@gmail.com>
2026-02-08 15:19:28 +0100

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT:
-// For GitHub Pages project sites, base must be "/<REPO_NAME>/"
-// Your screenshot shows the repo URL path as "Robofuture" (no "s").
-// If your repo name is different, change it below.
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/'
+  base: '/Robofutures/'
 })
